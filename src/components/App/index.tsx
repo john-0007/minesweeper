a86f72cbc52c12778b2171cbe2017a91baa98a10
John Joseph <atiquajohn@gmail.com>
2020-03-21 15:06:17 +0700

    finished styling body and row and cells

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,8 +1,29 @@
-import React from 'react'
+import React, { useState, ReactNode } from 'react'
+import NumberDisplay from '../NumberDisplay'
+import { generateCells } from '../../utils'
+import Button from '../Button'
+
 import './App.scss'
 
 const App = () => {
-	return <div className='App'>Hello form App</div>
+	const [cells, setCells] = useState(generateCells())
+	const renderCells = (): ReactNode => {
+		return cells.map((row, rowIndex) => row.map((col, colIndex) => <Button key={`${rowIndex}-${colIndex}`} />))
+	}
+	return (
+		<div className='App'>
+			<div className='Header'>
+				<NumberDisplay value={0} />
+				<div className='Face'>
+					<span role='img' aria-label='face'>
+						😀
+					</span>
+				</div>
+				<NumberDisplay value={23} />
+			</div>
+			<div className='Body'>{renderCells()}</div>
+		</div>
+	)
 }
 
 export default App
